c0fd4067ae2d42207512504bc133dbf601176fd7
nessux <eric.m.hamann@gmail.com>
2020-11-14 17:50:47 +0100

    Add project files.

---
+++ LeavingHome.cpp
@@ -0,0 +1,12 @@
+﻿// LeavingHome.cpp : Defines the entry point for the application.
+//
+
+#include "LeavingHome.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
